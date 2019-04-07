d0744a3e56a96d00a8bc919922faf9c40783ad7e
lzb6666 <zhongboge@foxmail.com>
2019-04-07 12:13:29 +0800

    server初始化

---
+++ server/onelist/src/main/java/com/whu/onelist/OnelistApplication.java
@@ -0,0 +1,13 @@
+package com.whu.onelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelistApplication.class, args);
+	}
+
+}
