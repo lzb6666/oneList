9c35dc9c4494ddec9d668e518b64aa91441c22aa
lzb6666 <zhongboge@foxmail.com>
2019-05-05 21:07:12 +0800

    Update restart.sh

--- server/restart.sh
+++ server/restart.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 SPRING_BOOT_JAR_NAME='onelist-0.0.1-SNAPSHOT.jar'
-PIDS=`ps -ef | grep "$SPRING_BOOT_JAR_NAME" | grep java |awk '{print $2}'`
+PIDS=`ps -ef |awk -v name=${SPRING_BOOT_JAR_NAME} '{if(($0~/name/)&&($0!~/awk/))print $2}'`
 if [ -z "$PIDS" ]; then
   echo "首次启动项目$SPRING_BOOT_JAR_NAME"
 fi
 for PID in $PIDS ; do
   kill $PID
 done
-java -jar /home/jenkins/projects/${SPRING_BOOT_JAR_NAME}
+nohup java -jar /home/jenkins/projects/$SPRING_BOOT_JAR_NAME > log 2>&1
+echo "启动完毕"
+cat log
